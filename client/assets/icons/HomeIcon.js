ae67f246e862a1f67396d01f498311fb9760c3c7
Maxim Mineev <mms1401@ya.ru>
2023-12-13 15:56:50 +0500

    Update HomeIcon.js

--- client/assets/icons/HomeIcon.js
+++ client/assets/icons/HomeIcon.js
@@ -4,16 +4,23 @@ import Svg, { Path } from "react-native-svg"
 function HomeIcon(props) {
     return (
         <Svg
-            xmlns="http://www.w3.org/2000/svg"
             width={24}
             height={24}
-            viewBox="0 0 20 22"
+            viewBox="0 0 24 24"
             fill="none"
+            xmlns="http://www.w3.org/2000/svg"
             {...props}
         >
             <Path
-                d="M1 8l9-7 9 7v11a2 2 0 01-2 2H3a2 2 0 01-2-2V8z"
-                stroke={props.color}
+                d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+                stroke="#fff"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <Path
+                d="M9 22V12h6v10"
+                stroke="#fff"
                 strokeWidth={2}
                 strokeLinecap="round"
                 strokeLinejoin="round"

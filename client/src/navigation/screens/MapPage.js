bc70f8d7542b960471bc91b2241d9f99f8bd8a70
Maxim Mineev <mms1401@ya.ru>
2024-01-06 21:18:57 +0500

    maps

--- client/src/navigation/screens/MapPage.js
+++ client/src/navigation/screens/MapPage.js
@@ -1,14 +1,36 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 import {useMyContext} from "../../../globalContext";
+import MapView, {Marker} from "react-native-maps";
+import { PROVIDER_GOOGLE } from "react-native-maps";
+import safeAreaViewAndroid from "../../components/SafeAreaViewAndroid";
+
+const styles = StyleSheet.create({
+    map: {
+        width: '100%',
+        height: '100%',
+    }
+})
 
 export default function MapPage({ navigation }) {
     const { globalID, setGlobalID } = useMyContext();
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Map" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Map Screen</Text>
-        </View>
+        <SafeAreaView style={safeAreaViewAndroid.AndroidSafeArea}>
+            <MapView
+                style={styles.map}
+                provider={PROVIDER_GOOGLE}
+                region={{
+                    latitude: 56.84392816904743,
+                    longitude: 60.65391892623551,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                <Marker coordinate={{
+                    latitude: 56.84392816904743,
+                    longitude: 60.65391892623551
+                }} />
+            </MapView>
+        </SafeAreaView>
     );
 }
